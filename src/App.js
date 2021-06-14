2b21b599dbd5791b96cb4cf06f604bdb6c6055fc
Jason Holloway <jjamal95@gmail.com>
2021-06-13 19:17:00 -0500

    finished checkout for paypal, added images for books, swapped mission statement  for vision statement, adding some styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import Carousel from "../src/components/Carousel";
 import { CarouselData } from "./components/CarouselData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 
 
@@ -17,9 +17,7 @@ function App() {
       <div className="side-bar" opacity="1">
         <Image className="logo" src="dalelogo.png" alt="queen" opacity="1" />
         <p>
-          Infinite Credit Success is dedicated to help as many families to
-          become Financial Educated with Financial Literacy tools and Build
-          Generational Wealth!
+        Raising the vibration of love through the many elements of infinity.
         </p>
         <div className="fbwrap" class="text-center">
                 <h1 class="text-center">Follow me on Facebook@ Infinite Credit Success</h1>
@@ -30,12 +28,34 @@ function App() {
                   <FontAwesomeIcon size="4x" opacity="1" icon={faFacebook} />
                 </a>
               </div>
+              <div className="instawrap" class="text-center">
+                <h1 class="text-center">Follow me on Instagram@ Elements of Infinity</h1>
+                <a class="text-center"
+                  href="https://www.instagram.com/elementsofinfinity/"
+                  className="instagram-social"
+                >
+                  <FontAwesomeIcon size="4x" opacity="1" icon={faInstagram} />
+                </a>
+              </div>
       </div>
       <div className="main">
         <div className="main-wrapper" opacity="1">
-          <h1 text-align="center">Elements Of Infinity</h1>
+          <h1 text-align="center">Infinite Credit Success</h1>
           <Image className="firstpic" src="dale.png" alt="queen" opacity="1" />
+          <p>
+          Infinite Credit Success is dedicated to help as many families to
+          become Financial Educated with Financial Literacy tools and Build
+          Generational Wealth!
+          </p>
         </div>
+          <div className="services">
+          {/* <a href="">
+  <Image src="aj.jpg" className="githubIcon" />
+</a> */}
+
+
+
+          </div>
       <div className="carousel">
       <Carousel  slides={CarouselData}/>
       
